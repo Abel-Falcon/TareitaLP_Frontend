99089d2f50d1469baf9c0d092a0264a7969d3791
Alumno Laboratorio L8 <alum.l8@lim.upeu.edu.pe>
2024-10-14 16:50:47 -0500

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(),
+    MessageService
+  ]
+
 };
